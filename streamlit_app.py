630cd037047c8b46bc6b6c5b5f3c64832fce7c9c
bbadzioch <badzioch@buffalo.edu>
2024-04-03 17:57:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -135,7 +135,7 @@ data = AgGrid(df,
               gridOptions=gridOptions,
               enable_enterprise_modules=True,
               update_mode=GridUpdateMode.SELECTION_CHANGED,
-              height=800
+              height=1000
              )
 st.session_state.selection = pd.DataFrame(data["selected_rows"]).iloc[:, 1:]
 
