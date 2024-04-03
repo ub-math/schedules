485237d50bbe417a1789e28b37edff055820167d
bbadzioch <badzioch@buffalo.edu>
2024-04-03 17:52:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -135,7 +135,7 @@ gridOptions = gb.build()
 
 data = AgGrid(df,
               gridOptions=gridOptions,
-              enable_enterprise_modules=True,
+              enable_enterprise_modules=False,
               update_mode=GridUpdateMode.SELECTION_CHANGED,
               #height=400
              )
